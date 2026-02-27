091a233ad7ed40c8f7e661386b01c740dc4196ac
helenrainbow2000kv <helenrainbow2000kv@outlook.com>
2026-02-27 18:01:58 +0800

    test(toolbox): escape reserved punctuation conservatively

--- tests/base64-url.test.js
+++ tests/base64-url.test.js
@@ -12,3 +12,7 @@ test('decodeBase64 rejects malformed input', () => {
   assert.throws(() => core.decodeBase64('a'), /Base64 格式不正确/);
   assert.throws(() => core.decodeBase64('!!!'), /Base64 格式不正确/);
 });
+test('encodeUrl escapes reserved punctuation conservatively', () => {
+  assert.equal(core.encodeUrl("a b&c!'()"), 'a%20b%26c%21%27%28%29');
+  assert.equal(core.decodeUrl('a%20b%26c%21%27%28%29'), "a b&c!'()");
+});
