2d3a0d738ddd8d08ba6b2f9eabc5128967d1238f
helenrainbow2000kv <helenrainbow2000kv@outlook.com>
2026-04-25 09:21:34 +0800

    test(toolbox): reject stray and unclosed csv quotes

--- tests/csv-quoting.test.js
+++ tests/csv-quoting.test.js
@@ -7,3 +7,8 @@ test('parseCsv handles quotes escapes and newline styles', () => {
   assert.deepEqual(core.parseCsv('a,b\r\n1,2'), [['a', 'b'], ['1', '2']]);
   assert.deepEqual(core.parseCsv('﻿a,b'), [['a', 'b']]);
 });
+test('parseCsv rejects stray and unclosed quotes', () => {
+  assert.throws(() => core.parseCsv('ab"c'), /CSV 引号位置不正确/);
+  assert.throws(() => core.parseCsv('"open'), /CSV 引号未闭合/);
+  assert.deepEqual(core.parseCsv(''), []);
+});
