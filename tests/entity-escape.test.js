c09b098ab10ecd34876bbb292f0ba9fa2ae2adce
helenrainbow2000kv <helenrainbow2000kv@outlook.com>
2026-08-01 13:59:33 +0800

    test(toolbox): round-trip entity escapes on plain text

--- tests/entity-escape.test.js
+++ tests/entity-escape.test.js
@@ -10,3 +10,7 @@ test('unescapeHtml resolves named and numeric references', () => {
   assert.equal(core.unescapeHtml('&amp;&lt;&#65;&#x41;&nbsp;'), '&<AA\u00a0');
   assert.equal(core.unescapeHtml('&#0;'), '\ufffd');
 });
+test('escape and unescape are inverses for plain text', () => {
+  const sample = 'plain text 123 你好';
+  assert.equal(core.unescapeHtml(core.escapeHtml(sample)), sample);
+});
